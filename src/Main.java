e78236b77f00b617099dce5ebb184cb5cd8c0300
Lk <lucasaraya19@hotmail.com>
2024-09-26 20:26:37 -0300

    Se creo view principal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import view.VentanaPrincipal;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                VentanaPrincipal ventana = new VentanaPrincipal();
+                ventana.setVisible(true);
+            }
+        });
     }
 }
